59d68c5a4377fcd32ba0d00d68e49c8ec208f29e
Utpal Patel <utpalpatel0055@gmail.com>
2024-10-14 23:41:05 -0400

    Exception handled

--- src/main/java/com/demo/Consult_Add/controller/ItemController.java
+++ src/main/java/com/demo/Consult_Add/controller/ItemController.java
@@ -6,6 +6,8 @@ import com.demo.Consult_Add.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,26 +19,30 @@ public class ItemController {
     public ItemService itemService;
 
     @GetMapping("/get-item/{id}")
-    public Item getItem(@PathVariable int id) {
+    public ResponseEntity<?> getItem(@PathVariable int id) {
         try {
             Item item = itemService.getItemById(id);
             if(item == null) {
                 Logger.info("Item not found");
             }
-            return item;
+            return new ResponseEntity<>(item, HttpStatus.OK);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            Logger.error("Error while getting item", e);
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/save-item")
-    public Item saveItem(@RequestBody Item item) {
+    public ResponseEntity<?> saveItem(@RequestBody Item item) {
         try {
             Item savedItem = itemService.saveItem(item);
             Logger.info("Item saved");
-            return savedItem;
+            return new ResponseEntity<>(savedItem, HttpStatus.OK);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            Logger.error("Error while creating item", e);
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
