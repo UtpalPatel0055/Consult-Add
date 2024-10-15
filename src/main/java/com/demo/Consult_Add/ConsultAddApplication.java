b37bb9d414676189239bd6e44b41c2d06dd5e33b
Utpal Patel <utpalpatel0055@gmail.com>
2024-10-14 23:14:48 -0400

    Init

---
+++ src/main/java/com/demo/Consult_Add/ConsultAddApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Consult_Add;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultAddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/Consult_Add/ConsultAddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Consult_Add;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
